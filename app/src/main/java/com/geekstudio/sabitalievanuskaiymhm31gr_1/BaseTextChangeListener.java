bd12ff7fce1659e0f63659f5f41b3dc28f050b39
SabitalievaNuskaiym <77196324+Sabitalieva@users.noreply.github.com>
2022-11-23 20:29:03 +0600

    Initial commit

---
+++ app/src/main/java/com/geekstudio/sabitalievanuskaiymhm31gr_1/BaseTextChangeListener.java
@@ -0,0 +1,21 @@
+package com.geekstudio.sabitalievanuskaiymhm31gr_1;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+public class BaseTextChangeListener implements TextWatcher {
+    @Override
+    public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable editable) {
+
+    }
+}
